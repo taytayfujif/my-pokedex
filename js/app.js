17f19e798d09cf25c5303aead8f5d92293701107
jhoun <jayhoun@gmail.com>
2017-09-26 16:00:54 -1000

    added css, js, and index.html

---
+++ js/app.js
@@ -0,0 +1,64 @@
+/*Use the Document Object Model (DOM) to complete the following exercises below:*/
+
+/*1. Taylor Swift
+
+Find the div with the id of "name1" and replace the n/a with the following:
+
+Tay-Tay*/
+
+document.getElementById("name1").innerHTML = "Tay Tay"
+
+
+/*2. DJ Khaled
+
+Find the div with the id of "position2" and replace the n/a with the following:
+
+Project Manager*/
+
+
+/*3. Piko Taro
+
+Find the div with the id of "alias3" and replace the n/a with the following:
+
+Concatenation*/
+
+
+
+
+/*4. Prince
+
+Find the div with the class name of "profile" and replace the n/a with a verse from your favorite Prince song.*/
+
+
+
+/*5. Bruce Lee
+
+Find the div with the class name of "profile" and replace the n/a with a quote from the legend himself.*/
+
+
+
+/*6. Samuel L Jackson
+
+Find the div with the class name of "alias" and replace the n/a with your favorite character that Sammy portrayed.*/
+
+
+
+/*7. Peter Griffin
+
+Create a div element and give it an id of "name7". Inside this div element, give it the contents of "Peter Griffin".
+
+Append this div element to the nameParent div*/
+
+
+
+/*8. Tim Duncan
+
+Create a div element give it an id of "alias8". Inside this div element, give it the contents of "Old Man Riverwalk".
+
+Append this div element to the aliasParent div.*/
+
+
+
+//Final Boss
+/*9. Create your own profile.*/
+

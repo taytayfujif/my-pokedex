7d95f86469f86902b489e92caeb2a11fec5fe158
taytayfujif <31020217+taytayfujif@users.noreply.github.com>
2017-12-06 14:28:44 -1000

    final commit

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@
 // Pokemon 1
 // Find the div with the id of "name1" and replace the text n/a with "Tentacool"
 // Place your code below
+document.getElementById("name1").innerHTML = "Tentacool"
 
 
 
 // Pokemon 2
 // Find the div with the id of "type2" and replace the text n/a with "water"
 // Place your code below
+document.getElementById("type2").innerHTML = "water"
 
 
 
@@ -17,7 +19,7 @@
 // Pokemon 3
 // Find the div with the id of "pokeNumber3" and replace the text n/a with 116:
 // Place your code below
-
+document.getElementById("pokeNumber3").innerHTML = "116"
 
 
 
@@ -25,6 +27,7 @@
 // Pokemon 4
 // Find the div with the id of "bio4" and replace the text n/a with your personal description of this Pokemon.
 // Place your code below
+document.getElementById("bio4").innerHTML ="kingler is a crab water type pokemon who has a weakness to grass and electricity"
 
 
 
@@ -33,7 +36,7 @@
 // Pokemon 5
 // Find the div with the id of "bio5" and replace the the font color to "red";
 // Place your code below
-
+document.getElementById("bio5").style.color = "red"
 
 
 
@@ -41,7 +44,7 @@
 // Pokemon 6
 // Find the div with the id of "name6" and replace the font size to "60px";
 // Place your code below
-
+document.getElementById("name6").style.fontSize = "60px"
 
 
 
@@ -49,5 +52,6 @@
 // Pokemon 7
 // Find the div with the id of "container" and change the background color to "green"
 // Place your code below
+document.getElementById("container").style.backgroundColor = "green"
 
 
